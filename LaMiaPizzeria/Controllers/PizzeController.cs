391e057ae82eb839cac08691ed01a3ce5795a385
davidepetrillo <davide96petrillo@gmail.com>
2022-05-11 23:24:11 +0200

    Aggiunto Details

--- LaMiaPizzeria/Controllers/PizzeController.cs
+++ LaMiaPizzeria/Controllers/PizzeController.cs
@@ -7,11 +7,34 @@ namespace LaMiaPizzeria.Controllers
     public class PizzeController : Controller
     {
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Index(string nome, string descrizione, string prezzo, string immagine)
         {
             List<Pizze> pizze = PizzeData.GetPizze();
 
             return View("HomePage", pizze);
         }
+
+        [HttpGet]
+
+        public IActionResult Details(int id)
+        {
+            Pizze pizzaTrovata = null;
+            foreach(Pizze pizza in PizzeData.GetPizze())
+            {
+                if(pizza.Id == id)
+                {
+                    pizzaTrovata = pizza;
+                    break;
+                }
+            }
+            if(pizzaTrovata != null)
+            {
+                return View("Details", pizzaTrovata)
+            } else
+            {
+                return NotFound("La pizza con l'id " + id + "non è stato trovato");
+            }
+        }
     }
+
 }
